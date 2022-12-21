94325ef9bccb83f45b3cf5cf0f04f94363b03203
mepavankumar15 <119803977+mepavankumar15@users.noreply.github.com>
2022-12-21 20:18:40 +0530

    #include<stdio.h> int main(){     int a,b=0,i;     scanf("%d",&a);     for(i=1;i<a;i++){         if(a%i==0){             b=b+i;         }     }         if(b==a){             printf("PERFECT");         }         else if(b<a){             printf("DEFICIENT");         }         else if(b>a){             printf("ABUNDANT");         }         else{             printf("not valid");         }
    }

---
+++ PDA.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int a,b=0,i;
+    scanf("%d",&a);
+    for(i=1;i<a;i++){
+        if(a%i==0){
+            b=b+i;
+        }
+    }
+        if(b==a){
+            printf("PERFECT");
+        }
+        else if(b<a){
+            printf("DEFICIENT");
+        }
+        else if(b>a){
+            printf("ABUNDANT");
+        }
+        else{
+            printf("not valid");
+        }
+    
+}
